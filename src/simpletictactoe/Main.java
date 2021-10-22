f4d616f5a49e429608fcc36c06b707c158169a4d
ane1123 <92170135+ane1123@users.noreply.github.com>
2021-10-22 20:02:01 +0500

    Update Main.java

--- src/simpletictactoe/Main.java
+++ src/simpletictactoe/Main.java
@@ -1,7 +1,118 @@
 package simpletictactoe;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Simple Tic-Tac-Toe");
+        public static boolean isWin = false;
+
+    public static void main(String[] args) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int randomPlayer = (int) (Math.random() * 2 + 1);
+        if (randomPlayer == 1)
+            System.out.println("Player1 ходит первым!" + '\n');
+        else System.out.println("Player2 ходит первым!" + '\n');
+
+        System.out.println("Введите число от 1 до 9, чтобы сделать Ваш ход: " + '\n');
+
+        char[][] field = {{'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}};
+
+        char crossOrZero;
+
+        int switcher = 0; // переключатель между первым и вторым игроком
+        while (true) {
+
+            if (switcher % 2 == 0)
+                crossOrZero = 'x';
+            else crossOrZero = 'o';
+
+            printField(field);
+
+            int num;
+            try {
+                num = Integer.parseInt(reader.readLine()); // запрет ввода символов, отличных от чисел
+            } catch (NumberFormatException | IOException e) {
+                System.out.println("Число, только число!");
+                continue;
+            }
+
+            if (Arrays.deepToString(field).contains(Integer.toString(num)) && num <= 9 && num > 0) {
+                switch (num) {
+                    case 1:
+                        field[0][0] = crossOrZero;
+                        break;
+                    case 2:
+                        field[0][1] = crossOrZero;
+                        break;
+                    case 3:
+                        field[0][2] = crossOrZero;
+                        break;
+                    case 4:
+                        field[1][0] = crossOrZero;
+                        break;
+                    case 5:
+                        field[1][1] = crossOrZero;
+                        break;
+                    case 6:
+                        field[1][2] = crossOrZero;
+                        break;
+                    case 7:
+                        field[2][0] = crossOrZero;
+                        break;
+                    case 8:
+                        field[2][1] = crossOrZero;
+                        break;
+                    case 9:
+                        field[2][2] = crossOrZero;
+                        break;
+                }
+            } else {
+                System.out.println("Введите число из оставшихся на поле!");
+                continue;
+            }
+
+            checkingForMatch(field);
+
+            if (isWin) {
+                System.out.println("YOU WIN!!!");
+                printField(field);
+                break;
+            }
+            switcher++;
+        }
+    }
+
+    public static void printField(char[][] field) { // печать поля
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (j != 2)
+                    System.out.print(field[i][j] + "|");
+                else System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void checkingForMatch(char[][] field) { // проверка на совпадение трёх элементов
+
+        for (int i = 0; i < field.length; i++) {
+            if (field[i][0] == field[i][1] && field[i][0] == field[i][2] || // сравнение по горизонтали
+                    field[0][i] == field[1][i] && field[0][i] == field[2][i] || // сравнение по вертикали
+                    field[0][0] == field[1][1] && field[0][0] == field[2][2] || // сравнение по
+                    field[2][0] == field[1][1] && field[2][0] == field[0][2]) { // диагоналям
+
+                isWin = true;
+                break;
+            }
+        }
     }
 }

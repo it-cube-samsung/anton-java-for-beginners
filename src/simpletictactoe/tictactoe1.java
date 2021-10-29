9632f883694bf0fa66bf064b3e51273307733cde
AizeRs <55633813+AizeRs@users.noreply.github.com>
2021-10-29 19:27:16 +0500

    tic tac full

---
+++ src/simpletictactoe/tictactoe1.java
@@ -0,0 +1,9 @@
+package simpletictactoe;
+
+public class tictactoe1 {
+    public static void main(String[] args) {
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O");
+    }
+}
